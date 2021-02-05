de9c8c1e18bc9b268bd51736095124c9423b7127
ahmad1091 <ahmadtayeb91@gmail.com>
2021-02-05 14:11:55 +0200

    create userLeave and getRooUsers utils

--- utils/users.js
+++ utils/users.js
@@ -1,7 +1,7 @@
 const users = [];
 
 const userJoin = (id, username, room) => {
-  const user = { is, username, room };
+  const user = { id, username, room };
   users.push(user);
   return user;
 };
@@ -10,4 +10,15 @@ const getCurrentUser = (id) => {
   return users.find((user) => user.id === id);
 };
 
-module.exports = { userJoin, getCurrentUser };
+const userLeave = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+const getRoomUsers = (room) => {
+  return users.filter((user) => user.room === room);
+};
+
+module.exports = { userJoin, getCurrentUser, userLeave, getCurrentUser };
