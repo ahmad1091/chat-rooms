eab82b2e514ee538334b37783ca4f7ec799e6863
ahmad1091 <ahmadtayeb91@gmail.com>
2021-02-05 13:06:02 +0200

    using formatMessges to return message object

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const formatMessage = require("./utils/messages");
 
 const port = 3000;
 io.on("connection", (socket) => {
-  socket.emit("message", "hello !!");
-  socket.broadcast.emit("message", "a user joined");
+  socket.emit("message", formatMessage("Admin", "wellcom!!"));
+  socket.broadcast.emit("message", formatMessage("Admin", "user joined"));
 
   socket.on("chatMessage", (msg) => {
-    socket.emit("message", msg);
+    socket.emit("message", formatMessage("user", msg));
   });
 
   socket.on("disconnect", () => {
-    io.emit("message", "a user disconnected");
+    io.emit("message", formatMessage("Admin", "user disconnected"));
   });
 });
 app.use(express.static("public"));
