a8a0ffaf8803223eb87ce253fbe08bc7ef3f1de3
ahmad1091 <ahmadtayeb91@gmail.com>
2021-02-05 12:49:11 +0200

    update message functionalty and dom in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,18 @@
 const socket = io("/");
 const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
 
 socket.on("message", (msg) => {
   renderMessage(msg);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const msg = e.target.elements.msg.value;
   socket.emit("chatMessage", msg);
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
 });
 
 const renderMessage = (msg) => {
